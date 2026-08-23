83dbdbadae7c6ecf93cbd576702908de23549438
sharanyakulal17 <sharanyakulal17@gmail.com>
2026-08-23 10:57:55 +0530

    updated rock_paper_scissor.py

--- project_3/Rock_Paper_sccisor.py
+++ project_3/Rock_Paper_sccisor.py
@@ -1,31 +1,61 @@
 import random
 choice= ["rock", "paper", "scissor"]
 
-def valid_input():
+def valid_name():
+     while True:
+        name = input("Enter your name:")
+        try:
+            name = int(name)
+            print("Please enetr valid input")
+            continue
+        except ValueError:
+            return name
+
+def valid_input(name, count):
     while True:
-        user_choice = input("Enter your choice(rock, paper, scissor):")
+        user_choice = input(f"Enter your choice(rock, paper, scissor) chance:{count}:")
         if user_choice not in choice:
-            print("please enter valid choice")
+            print(f"{name} please enter valid choice")
         else:
             return user_choice
-        
-def rock_paper_scissor():
-    while True:
-        comp_gen = random.choice(choice)
-        user_choice = valid_input()
-        if(comp_gen == user_choice):
-            print("Its a tie")
-            print(f"user choice -> {user_choice} and comp generated -> {comp_gen}")
-        elif (comp_gen == "rock" and user_choice == "scissor") or (comp_gen == "paper" and user_choice == "rock") or (comp_gen == "scissor" and user_choice == "paper"):
-            print("computer wins")
-        else:
-            print("user wins")
-        break
 
+def rock_paper_scissor(name, score, history):
+    count = 1
+    print("You have 4 chance to play")
+    while count <= 4:
+        comp_gen = random.choice(choice)
+        user_choice = valid_input(name, count)
+        count += 1
+
+        if(comp_gen == user_choice):
+            result = f"Its a tie {name}"
+            print(result)
+            score["tie"] += 1
+        elif (comp_gen == "rock" and user_choice == "scissor") or (comp_gen == "paper" and user_choice == "rock") or (comp_gen == "scissor" and user_choice == "paper"):
+            result = "computer wins"
+            print(result)
+            score["loses"] += 1
+        else:
+            result = f"{name} wins"
+            print(result)
+            score["wins"] += 1
+        print(f"your choice -> {user_choice} and comp generated -> {comp_gen}")
+        print(f"tie: {score["tie"]}, lose: {score["loses"]}, win: {score["wins"]}")
+        history.append(f"user choice -> {user_choice} and comp generated -> {comp_gen} ----------> {result}")
+    return name
+
+history= []
+name = valid_name()
+score = {"tie":0, "loses":0, "wins":0}
 while True:
-    rock_paper_scissor()
-    again = input("Do you want to play again? (y/n):")
+    player_name = rock_paper_scissor(name, score, history)
+    again = input(f"Do you want to play again? (y/n) {player_name}:")
 
     if again.lower() != "y":
-        print("Thankyou for playing")
+        print(f"Thankyou for playing {player_name}")
+        print("final score:")
+        print(f"tie: {score["tie"]}, lose: {score["loses"]}, win: {score["wins"]}")
+        print("History:")
+        for entry in history:
+            print(entry)
         break
